63a77657b73c43cfdbf7469dee1ac716aeb35c2a
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-10-14 01:01:13 +1000

    project start

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import logo from "../../assets/imgs/header/logo.png"
+
+const Header = () => {
+    return (
+        <div>
+            <img src={logo} alt="logo" />
+        </div>
+    );
+}
+ 
+export default Header;
